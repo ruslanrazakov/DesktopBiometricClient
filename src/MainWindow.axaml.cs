ea0e82b98486637e0623c4bd2248e122d2d25ddd
koobalack <koobalack@gmail.com>
2022-12-12 00:06:51 +0300

    Avalonia template created

---
+++ src/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FaceRecognitionClient;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
